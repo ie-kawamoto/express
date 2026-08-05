034ade19b02b7ebc989068dc40ab4894eabef738
ie-kawamoto <kawamonofficial@gmail.com>
2026-08-05 23:50:52 +0900

    第14回演習

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+const cors = require('cors'); // corsミドルウェアを追加
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
+
+if (!uri || !/^mongodb(\+srv)?:\/\//.test(uri)) {
+  throw new Error(
+    'MONGODB_URI must be set to a valid MongoDB connection string ' +
+    '(starting with "mongodb://" or "mongodb+srv://")'
+  );
+}
 const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
-// idが１のドキュメントを取得
-const query = { id: 1 };
-const note = await notes.findOne(query);
+
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
+
 res.json(note);
 })
+
 module.exports = router;
