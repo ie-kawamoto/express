94991beb0332dedfe0106c55f8fea812ec9a9957
ie-kawamoto <kawamonofficial@gmail.com>
2026-08-06 00:17:11 +0900

    演習　修正

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
-
-if (!uri || !/^mongodb(\+srv)?:\/\//.test(uri)) {
-  throw new Error(
-    'MONGODB_URI must be set to a valid MongoDB connection string ' +
-    '(starting with "mongodb://" or "mongodb+srv://")'
-  );
-}
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
